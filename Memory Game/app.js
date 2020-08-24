983c895a06ad5309e74d2027c354bb055d6b6f22
Jolie Adam <as.jolie3@gmail.com>
2020-08-24 19:43:58 -0400

    creating game board

--- Memory Game/app.js
+++ Memory Game/app.js
@@ -51,8 +51,18 @@ document.addEventListener('DOMContentLoaded', () => {
   ]
  cardArray.sort(() => 0.5 - Math.random())
 
-  const grid = document.querySelector('.grid')
-  const resultDisplay = document.querySelector('#result')
-  var cardsChosen = []
-  var cardsChosenId = []
-  const cardsWon = []
+  const grid = document.querySelector('.grid');
+  const resultDisplay = document.querySelector('#result');
+  var cardsChosen = [];
+  var cardsChosenId = [];
+  const cardsWon = [];
+
+function createBoard() {
+    for (let i = 0; i < cardArray.length; i++) {
+      var card = document.createElement('img');
+      card.setAttribute('src', 'images/blank.png');
+      card.setAttribute('data-id', i);
+      card.addEventListener('click', flipCard);
+      grid.appendChild(card);
+    }
+  }
